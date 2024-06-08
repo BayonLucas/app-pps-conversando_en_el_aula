85aea97b2941db1e6c84033d2632d273f62ed377
BayonLucas <lucas.bayon31@hotmail.com>
2024-06-08 11:30:30 -0300

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app-conversando-en-el-aula',
+  appId: 'com.conversando_en_el_aula.app',
+  appName: 'app',
   webDir: 'www'
 };
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,28 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'splash-screen',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'splash-screen',
+    loadComponent: () => import('./pages/splash-screen/splash-screen.page').then( m => m.SplashScreenPage)
+  },
+  {
+    path: 'chat-a',
+    loadComponent: () => import('./pages/chat-a/chat-a.page').then( m => m.ChatAPage)
+  },
+  {
+    path: 'chat-b',
+    loadComponent: () => import('./pages/chat-b/chat-b.page').then( m => m.ChatBPage)
   },
 ];
